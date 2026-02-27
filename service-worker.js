2e722da5560126aa03aa462e5d58fc03ef30483e
moodx5040 <moodx5040@gmail.com>
2026-02-27 16:15:15 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "novarin-cache-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./elite.html",
+  "./docs.html",
+  "./branches.html",
+  "./channels.html",
+  "./manifest.json",
+  "./افتار.png",
+  "./16.jpg",
+  "./17.jpg",
+  "./18.jpg",
+  "./مقدمه.jpg",
+  "./فيد.mp4"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
